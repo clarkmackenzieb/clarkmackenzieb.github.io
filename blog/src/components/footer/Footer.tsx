6aaddc8acfcbba6ab91ef6cdd58422771de3cc84
Kenzie <clarkmackenzieb@gmail.com>
2021-10-11 14:11:19 -0500

    add footer file

---
+++ blog/src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer(){
+  return (
+    <div>
+      <div>
+        github link
+      </div>
+      <div>
+        linked in link
+      </div>
+    </div>
+  );
+}
