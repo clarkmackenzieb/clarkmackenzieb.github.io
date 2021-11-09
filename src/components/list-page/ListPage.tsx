2a173005d4edc34d5c19bd546719eb2f51a55b00
mackenzie.clark <mackenzie.clark@match.com>
2021-11-09 13:25:11 -0600

    remove console log

--- src/components/list-page/ListPage.tsx
+++ src/components/list-page/ListPage.tsx
@@ -42,7 +42,6 @@ export default function ListPage({
   if(postList && postList.length === 0){
     return renderZeroState();
   }
-  console.log("link", link)
   const list = postList.map((post) => {
     return (
       <StyledListItem key={post.title}>
